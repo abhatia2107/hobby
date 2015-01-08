efe3755e5982711cc06a1c1fc1dd9122dd6933fa
harikrishna <harikrishna.vanjari@gmail.com>
2015-01-08 23:10:44 +0530

    home page modified

--- app/views/Templates/head.blade.php
+++ app/views/Templates/head.blade.php
@@ -10,7 +10,8 @@
     <link rel="stylesheet" type="text/css" href="/assets/css/normalize-3.0.2.min.css">
     <link rel="stylesheet" type="text/css" href="/assets/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="/assets/css/jquery-ui-1.11.2.min.css" id="jquery-ui-style-css" media="all">
-    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+    <!--<link rel="stylesheet" type="text/css" href="/assets/css/style.css"> -->
+    <link rel="stylesheet" type="text/css" href="/assets/css/hobby_style.css">
     <script src="/assets/js/jquery-1.11.2.min.js"></script>
     <script src="/assets/js/jquery-2.1.1.min.js"></script>
     <script src="/assets/js/jquery-ui-1.10.4.min.js"></script>

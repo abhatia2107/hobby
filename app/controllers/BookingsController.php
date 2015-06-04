f05bfbaec75db3abfb1ee4bf7428e571f14ae755
abhatia2107 <abhatia2107@gmail.com>
2015-06-04 17:26:18 +0530

    Iframe to be loaded.

--- app/controllers/BookingsController.php
+++ app/controllers/BookingsController.php
@@ -72,14 +72,21 @@ class BookingsController extends \BaseController {
 		$posted['cancel_url']=url('/bookings/cancel');
 		$posted['integration_type']='iframe_normal';
 		$posted['language']='en';
-
+		$posted['billing_name']='Abhishek Bhatia';
+		$posted['billing_address']='Room no 1101, near Railway station Ambad';
+		$posted['billing_city']='Indore';
+		$posted['billing_state']='MP';
+		$posted['billing_zip']='425001';
+		$posted['billing_country']='India';
+		$posted['billing_tel']='9876543210';
+		$posted['billing_email']='test@test.com';
 		foreach ($posted as $key => $value){
 			$merchant_data.=$key.'='.$value.'&';
 		}
 		$encrypted_data=encrypt($merchant_data,$working_key); // Method for encrypting the data.
+		$action='https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction&encRequest='.$encrypted_data.'&access_code='.$access_code;
 		
-	    $action='https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction&encRequest=92f6d327c0d5a857392e06af22fc2c7bb9f7e45d4aa31f1c5b1eb09866adb87f916f62285e31552ac75fa5d0ed25b00c7faa3662a205300a4e1209a26ce02c8d3d73204ea7421ac46d909c086533039b7143678b59bae36c4a1859b094635271f011829ba4768d6581b940546eb1aa7d7b70a140df5dcb177e34e78525c7ac664cf689cb2a60dc3f7b18d315e92f5daf0f51248820c1cb20fd694758de8e8d482af313f055472b92c76a9b4bd646ce02d941d78c76eb0640091ce0f0695429ceb6366ef4e1632f28e7dfc66d9f116cdbdf6add993e1c6684fab410694fbe5650&access_code=AVCA05CE22BS53ACSB';
-		return View::make('Bookings.iframe',compact('encrypted_data','access_code'));
+		return View::make('Bookings.iframe',compact('action'));
 	}
 	/**
 	 * Store a newly created booking in storage.
@@ -88,8 +95,32 @@ class BookingsController extends \BaseController {
 	 */
 	public function store()
 	{
-		$action=Input::get('action');
-		return View::make('Bookings.iframe',compact('action'));
+		include app_path().'/IFRAME_KIT/Crypto.php';
+		$merchant_id='61787';
+		$working_key='AEB6A7302F8DC5AC50A53B8DED9FB9DF';
+		$access_code='AVCA05CE22BS53ACSB';
+		$merchant_data='';
+		
+		$user_id=Auth::id();
+		$user=User::find($user_id);
+		$order_id = substr(hash('sha256', mt_rand() . microtime()), 0, 20);
+	    $posted['merchant_id']=$merchant_id;
+		$posted['order_id']=$order_id;
+		$posted['currency']='INR';
+		$posted['amount']=50;
+		$posted['redirect_url']=url('/bookings/redirect');
+		$posted['cancel_url']=url('/bookings/cancel');
+		$posted['integration_type']='iframe_normal';
+		$posted['language']='en';
+
+		foreach ($posted as $key => $value){
+			$merchant_data.=$key.'='.$value.'&';
+		}
+		$encrypted_data=encrypt($merchant_data,$working_key); // Method for encrypting the data.
+		
+	    $action='https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction&encRequest=92f6d327c0d5a857392e06af22fc2c7bb9f7e45d4aa31f1c5b1eb09866adb87f916f62285e31552ac75fa5d0ed25b00c7faa3662a205300a4e1209a26ce02c8d3d73204ea7421ac46d909c086533039b7143678b59bae36c4a1859b094635271f011829ba4768d6581b940546eb1aa7d7b70a140df5dcb177e34e78525c7ac664cf689cb2a60dc3f7b18d315e92f5daf0f51248820c1cb20fd694758de8e8d482af313f055472b92c76a9b4bd646ce02d941d78c76eb0640091ce0f0695429ceb6366ef4e1632f28e7dfc66d9f116cdbdf6add993e1c6684fab410694fbe5650&access_code=AVCA05CE22BS53ACSB';
+		return View::make('Bookings.iframe',compact('encrypted_data','access_code'));
+	
 /*		dd(Input::all());
 		$validator = Validator::make($data = Input::all(), Booking::$rules);
 
@@ -107,8 +138,6 @@ class BookingsController extends \BaseController {
 
 	public function redirect()
 	{
-		// dd('passed');
-	
 		include app_path().'/IFRAME_KIT/Crypto.php';
 		error_reporting(0);
 		$working_key='AEB6A7302F8DC5AC50A53B8DED9FB9DF';
@@ -122,11 +151,11 @@ class BookingsController extends \BaseController {
 			$information=explode('=',$decryptValues[$i]);
 			if($i==3)	$order_status=$information[1];
 		}
+		dd($order_status);	
 
 		if($order_status==="Success")
 		{
 			echo "Thank you for shopping with us. Your credit card has been charged and your transaction is successful. We will be shipping your order to you soon.";
-			
 		}
 		else if($order_status==="Aborted")
 		{

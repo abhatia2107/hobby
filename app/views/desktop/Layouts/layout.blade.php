8fd628c943ea73e35a5b3b76e6e8b8251deeecc6
harikrishna <harikrishna.vanjari@gmail.com>
2015-06-13 00:34:49 +0530

    Filter page fb like button integrated.

--- app/views/desktop/Layouts/layout.blade.php
+++ app/views/desktop/Layouts/layout.blade.php
@@ -4,7 +4,16 @@
 		@include('Templates.head')
 	    @yield('pagestylesheet')
 	</head>	
-	<body> 		
+	<body> 	
+		<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=787415747998638";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+	
 		<!--Header Section contains sign-in sign-up searchbox and logo -->			
 		{{--
 		<header class="layout_header" >

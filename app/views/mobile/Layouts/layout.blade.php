f292d0b9b80e5c928afccca5c2c33a89a4d98a05
abhatia2107 <abhatia2107@gmail.com>
2015-06-13 02:36:17 +0530

    Google analytics added to the website.

--- app/views/mobile/Layouts/layout.blade.php
+++ app/views/mobile/Layouts/layout.blade.php
@@ -5,6 +5,26 @@
 	    @yield('pagestylesheet')
 	</head>	
 	<body> 		
+		<!-- Facebook SDK code -->
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=787415747998638";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+		<!-- Google Analytics code -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-64069425-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+
 		<!--Header Section contains sign-in sign-up searchbox and logo -->			
 		{{--
 		<header class="layout_header" >

5bc76ac90e474b5693d1f6f36b7fde48946c4a9e
abhatia2107 <abhatia2107@gmail.com>
2015-06-19 23:34:00 +0530

    Redirect added for www.hobbyix.com

--- app/filters.php
+++ app/filters.php
@@ -15,7 +15,7 @@
 
 App::before(function($request)
 {
-	if ((Request::root()=== 'http://hobbyix.in')||(Request::root()=== 'http://www.hobbyix.in'))
+	if ((Request::root()=== 'http://hobbyix.in')||(Request::root()=== 'http://www.hobbyix.in')||(Request::root()=== 'http://www.hobbyix.com'))
 	// if ((Request::root()=== 'http://localhost:8000'))
 	{
 		return Redirect::to('http://hobbyix.com/'.Request::path());

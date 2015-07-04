51bc88b983656b83af4ebdae269fa5027e33653d
harikrishna <harikrishna.vanjari@gmail.com>
2015-07-04 16:48:12 +0530

    fb:admins meta tag

--- app/views/desktop/Templates/head.blade.php
+++ app/views/desktop/Templates/head.blade.php
@@ -29,7 +29,7 @@
     <meta property="og:image" content="{{$facebookContent[2]}}" />
     <meta property="og:description" content="{{$facebookContent[3]}}" />
     <meta property="fb:app_id" content="1061701537179263" />
-    <meta property="fb:page_id" content="836049089804962" />
+    <meta property="fb:page-id" content="836049089804962" />
     <link rel="canonical" href="{{$facebookContent[1]}}">
     <link rel="icon" type="image/png" href="/assets/images/hobbyix_favicon.png">
     <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css">
